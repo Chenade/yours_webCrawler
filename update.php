8e93331591b5fe10774fa4ca765aea900693c0e1
YANGCHI KUO <chenade0312@gmail.com>
2024-04-09 11:20:02 +0200

    add price range

--- update.php
+++ update.php
@@ -20,12 +20,14 @@ function connectToDatabase() {
 
 // Function to insert data into the 'meals' table
 function insertMeals($conn, $restaurant_id, $menu) {
+    $min = -1;
+    $max = -1;
     foreach ($menu as $item) {
-        $tag = $item['category_name'];
-        $name = $item['name'];
-        $price = $item['price'];
-        $img = key_exists('img', $item) ? $item['img'] : null;
-        $description = key_exists('description', $item) ? $item['description'] : null;
+        $tag =  mysqli_real_escape_string($conn, $item['category_name']);
+        $name = mysqli_real_escape_string($conn, $item['name']);
+        $price = intval($item['price']);
+        $img = key_exists('food_image_url', $item) ? mysqli_real_escape_string($conn, $item['food_image_url']) : null;
+        $description = key_exists('product_description', $item) ? mysqli_real_escape_string($conn, $item['product_description']) : null;
         $created_at = date('Y-m-d H:i:s');
         $updated_at = date('Y-m-d H:i:s');
 
@@ -34,6 +36,14 @@ function insertMeals($conn, $restaurant_id, $menu) {
 
         try{
             $conn->query($sql);
+
+            if ($min == -1 || $price < $min) {
+                $min = $price;
+            }
+            if ($price > $max) {
+                $max = $price;
+            }
+
         } catch (Exception $e) {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
@@ -43,12 +53,13 @@ function insertMeals($conn, $restaurant_id, $menu) {
         //     echo "Error: " . $sql . "<br>" . $conn->error;
         // }
     }
+    $sql = 'UPDATE `restaurant` SET `min_price` = ' . $min . ', `max_price` = ' . $max . ' WHERE `id` = ' . $restaurant_id;
 }
 
 // Function to insert data into the 'restaurant' table
 function insertRestaurant($conn, $restaurant) {
+    
     $name = mysqli_real_escape_string($conn, $restaurant['name']);
-
     // ckeck if the restaurant exists with name
     $sql = "SELECT * FROM `restaurant` WHERE `name` = '" . $name . "'";
     $result = $conn->query($sql);
@@ -65,7 +76,7 @@ function insertRestaurant($conn, $restaurant) {
     $service_hours_text = mysqli_real_escape_string($conn, $restaurant['service_hours_text']);
     $announcement = mysqli_real_escape_string($conn, $restaurant['announcement']);
     $delivery_rules = mysqli_real_escape_string($conn, json_encode($restaurant['delivery_rules'] ?? '', JSON_UNESCAPED_UNICODE));
-    $thumbnailImageUrl = mysqli_real_escape_string($conn, $restaurant['thumbnailImageUrl'] ?? '');
+    $thumbnailImageUrl = mysqli_real_escape_string($conn, $restaurant['thumbnailImageUrl'] ?? '/images/meals-table.png'); 
     $note = mysqli_real_escape_string($conn, $restaurant['note']);
     $created_at = date('Y-m-d H:i:s');
     $updated_at = date('Y-m-d H:i:s');
@@ -100,6 +111,16 @@ foreach ($data as $restaurant) {
     if ($restaurant_id !== null) {
         // Insert meals for the restaurant
         insertMeals($conn, $restaurant_id, $restaurant['menu']);
+    }
+}
+
+// count id group by note
+$sql = "SELECT note, COUNT(id) as count FROM restaurant GROUP BY note";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    echo "Note count\n";
+    while($row = $result->fetch_assoc()) {
+        echo $row['note'] . ": " . $row['count'] . "\n";
     }
 }
 
